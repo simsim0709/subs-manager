6184a7dc32fe39281ab9497a33678fa952915fab
Ilkwon Sim <sim@theloho.com>
2017-02-01 10:34:24 +0900

    Change fast-render package from meteorhacks to staringatlights

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  "summary": "Subscriptions Manager for Meteor",
-  "version": "1.6.4",
-  "git": "https://github.com/meteorhacks/subs-manager.git",
-  "name": "meteorhacks:subs-manager"
+  summary: 'Subscriptions Manager for Meteor',
+  version: '1.6.4_1',
+  git: 'https://github.com/simsim0709/subs-manager.git',
+  name: 'thelohoadmin:subs-manager',
 });
 
-Package.on_use(function(api) {
+Package.on_use(function (api) {
   configurePackage(api);
+
   api.export(['SubsManager']);
 });
 
-Package.on_test(function(api) {
+Package.on_test(function (api) {
   configurePackage(api);
 
   api.use(['tinytest', 'mongo-livedata', 'session'], ['client', 'server']);
@@ -21,18 +22,18 @@ Package.on_test(function(api) {
   api.add_files([
     'tests/options.js',
     'tests/core.js',
-    'tests/error.js'
+    'tests/error.js',
   ], ['client']);
 });
 
 function configurePackage(api) {
-  if(api.versionsFrom) {
+  if (api.versionsFrom) {
     api.versionsFrom('METEOR@0.9.0');
   }
-  
+
   api.use(['deps', 'underscore', 'ejson'], ['client', 'server']);
-  api.use('meteorhacks:fast-render@2.7.0', 'client', {weak: true});
-  
+  api.use('staringatlights:fast-render@2.16.3', 'client', { weak: true });
+
   api.add_files([
     'lib/sub_manager.js',
   ], ['client', 'server']);
